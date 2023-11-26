b5186040e1a505ffcfd846920fb486b578914576
1harsh2009 <deekshasanghmitra1108@gmail.com>
2023-11-26 19:17:30 +0530

    dh

--- routes/index.js
+++ routes/index.js
@@ -5,45 +5,53 @@ const DpModel = require('./dp')
 const passport = require('passport');
 const localstrategy = require('passport-local').Strategy;
 const multer = require('multer');
-const dp = require('./dp');
 const postModel = require("./post")
+const upload2 = require("./multer")
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `./public/images/`); // Replace 'your_upload_folder_path' with the actual path
   },
-  filename: function (req, file, cb) {
+  filename: async function (req, file, cb) {
     var fl = file.mimetype
     var vd = fl.split('/')
 
     // }); // Adjust the path as needed
-
-  if (req.isAuthenticated() && req.user && req.user.username) {
-      const Dpdata = new DpModel({
-        username: req.user.username,
-        dp: req.user.username + "." + vd[1]
-    });
-
-    DpModel.create(Dpdata)
-        .then((registereduser) => {
-            console.log(registereduser);
-            // Handle success, such as sending a success response to the client
-            
-        })
-        .catch((err) => {
-            console.error(err);
-            // Handle the error, such as sending an error response to the client
-            
+      var hgo = await DpModel.findOne({username:req.user.username})
+      console.log(hgo);
+      if (hgo == null){
+        if (req.isAuthenticated() && req.user && req.user.username) {
+          const Dpdata = new DpModel({
+            username: req.user.username,
+            dp: req.user.username + "." + vd[1]
         });
-    } 
-    else {
-    // Handle case where user is not authenticated or does not have a username
-    // res.status(401).send("Unauthorized");
-  }
-
-
-    cb(null, `${req.user.username}`+"."+vd[1]);
+        DpModel.create(Dpdata)
+          .then((registereduser) => {
+              console.log(registereduser);
+              // Handle success, such as sending a success response to the client
+              
+          })
+          .catch((err) => {
+              console.error(err);
+              // Handle the error, such as sending an error response to the client
+              
+          });
+        } 
+        else {
+      // Handle case where user is not authenticated or does not have a username
+        res.status(401).send("Unauthorized");
+      }
+      cb(null, `${req.user.username}`+"."+vd[1]);
+      }
+      else if (hgo != null){
+        DpModel.updateOne(
+          { username:req.user.username },
+          { $unset: { dp: `${req.user.username}`+"."+vd[1] } }
+       )
+       cb(null, `${req.user.username}`+"."+vd[1]);
+      }
+      
   }
 });
 
@@ -53,7 +61,7 @@ router.use(express.urlencoded({ extended: false }));
 passport.use(new localstrategy(usersModel.authenticate()));
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',error:req.flash('error') });
 });
 
 router.get('/upload', (req, res) => {
@@ -65,7 +73,7 @@ router.post('/upload', upload.single('file'), (req, res) => {
     return res.status(404).send('No file uploaded.');
   }
 
-  console.log('File uploaded:', req.file);
+  // console.log('File uploaded:', req.file);
   // res.status(200).send('File uploaded!');
   res.redirect('/profile')
 });
@@ -76,19 +84,29 @@ router.post('/register', (req, res) => {
     password: req.body.password,
     email: req.body.email
   });
-
-  usersModel.register(userData, req.body.password).then((registereduser) => {
-    passport.authenticate("local")(req, res, () => {
-      res.redirect('/profile');
+  
+  usersModel.register(userData, req.body.password)
+    .then((registereduser) => {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect('/profile');
+      });
+    })
+    .catch((err) => {
+      // Handle registration errors here
+      console.error("Registration error:", err);
+      // You can send an appropriate response to the client, redirect to an error page, etc.
+      res.status(500).send("Registration failed. Please try again.");
+      
+      // req.flash("error","username already exist")
+      // res.redirect("/")
     });
-  });
+  
 });
 
 router.get('/profile', isLoggedIn, async function(req,res){
   var vc = await DpModel.findOne({username:req.user.username})
   var bk = ""
-  var lp = usersModel.countDocuments({}, { hint: "_id_" });
-  console.log(lp);
+  
   if (vc==null)
   {
     bk = "https://i0.wp.com/shortshayari.in/wp-content/uploads/2022/11/whatsapp-dp-100.webp"
@@ -112,16 +130,25 @@ router.get('/log', (req, res) => {
   res.render('log');
 });
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+router.post('/logout',isLoggedIn, (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.redirect('/');
+  });
 });
-
+router.post("/createpost",isLoggedIn,upload2.single('post-img'),(req,res,next) =>{
+  if(!req.file){
+    return res.status(400).send('no file uploaded')
+  }
+  res.redirect("/profile")
+})
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  
 }
 
 module.exports = router;
